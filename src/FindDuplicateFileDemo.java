d7441721fb4ddbc77c5b83772ea8bd46aaddebe7
pumba30 <pumba303030@yandex.ru>
2014-12-27 11:54:14 +0200

    v.0.0.94 переработанная версия сравнение по имени, размеру, хешу

---
+++ src/FindDuplicateFileDemo.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+/**
+ * Created by pumba30 on 25.12.2014.
+ */
+public class FindDuplicateFileDemo {
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+
+
+        SearchDuplicatesFiles searchDuplicatesFiles = new SearchDuplicatesFiles("c:/testing");
+        ArrayList<File> listFoundFiles = searchDuplicatesFiles.getFilesInDirectory();
+
+
+
+
+
+        System.out.println("Файлы найденные в директории");
+        for (File item : listFoundFiles) {
+            System.out.println(item);
+
+        }
+
+
+
+
+
+
+    }
+}
